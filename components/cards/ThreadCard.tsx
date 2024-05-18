25b956ef1b9994974368c895f3a6e6798401f145
muhammed Borham <muhammeddamiinn@gmail.com>
2024-05-18 14:43:25 +0300

    completed backend comment threads

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -34,9 +34,13 @@ const ThreadCard = ({
   community,
   createdAt,
   comments,
+  isComment,
 }: Props) => {
   return (
-    <article className='flex w-full flex-col rounded-xl bg-dark-2 p-7'>
+    <article
+      className={`flex w-full flex-col rounded-xl mt-4
+    ${isComment ? "px-0 xs:px-7" : "bg-dark-2 p-7"}
+    `}>
       <div className='flex items-start justify-between'>
         <div className='flex w-full flex-1 flex-row gap-4'>
           <div className='flex flex-col items-center'>
@@ -65,16 +69,16 @@ const ThreadCard = ({
                   width={24}
                   height={24}
                   className=' cursor-pointer object-contain'
-                              />
-                              <Link href={`/thread/${id}`}>   
-                <Image
-                  src='/assets/reply.svg'
-                  alt='reply'
-                  width={24}
-                  height={24}
-                  className=' cursor-pointer object-contain'
                 />
-                              </Link>
+                <Link href={`/thread/${id}`}>
+                  <Image
+                    src='/assets/reply.svg'
+                    alt='reply'
+                    width={24}
+                    height={24}
+                    className=' cursor-pointer object-contain'
+                  />
+                </Link>
                 <Image
                   src='/assets/repost.svg'
                   alt='repost'
@@ -89,8 +93,14 @@ const ThreadCard = ({
                   height={24}
                   className=' cursor-pointer object-contain'
                 />
-                          </div>
-                          
+              </div>
+              {isComment && comments.length > 0 && (
+                <Link href={`/thread/${id}`}>
+                  <p className='mt-1 text-subtle-medium text-gray-1'>
+                    {comments.length} replies
+                  </p>
+                </Link>
+              )}
             </div>
           </div>
         </div>
